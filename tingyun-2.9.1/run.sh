a5c6bfa3da7f3138668a987737acd5af5be87cad
JJmomark <kyukou@163.com>
2018-12-12 16:25:41 +0800

    add-php-tingyun

--- tingyun-2.9.1/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-tingyunappname=${appname}
-tingyunkey=key
-sed -i "3a nb_license=${tingyunkey}" /usr/lib/networkbench/networkbench-install.sh
-sed -i "4a nb_appname=${tingyunappname}" /usr/lib/networkbench/networkbench-install.sh
-sh /usr/bin/networkbench-install.sh install
-
-/usr/local/bin/docker-php-entrypoint php-fpm
